37f66637136de5a67ebe357924a94822ffbd0522
Ashwani Mishra <developer6.afluex@gmail.com>
2023-10-07 11:32:14 +0530

    'changes'

--- DreamCrusher/Models/DBHelper.cs
+++ DreamCrusher/Models/DBHelper.cs
@@ -16,8 +16,8 @@ namespace DreamCrusher.Models
         {
             try
             {
-                connectionString = "Data Source=103.48.51.111,1232;Initial Catalog=dreamcrusher;User Id=dreamuser;Password=Dre@m*_;Integrated Security=false;";
-                //connectionString = "Data Source=103.48.51.111,1232;Initial Catalog=dreamcrusherdbtest;User Id=dreamuser;Password=Dre@m*_;Integrated Security=false;";
+                //connectionString = "Data Source=103.48.51.111,1232;Initial Catalog=dreamcrusher;User Id=dreamuser;Password=Dre@m*_;Integrated Security=false;";
+                connectionString = "Data Source=103.48.51.111,1232;Initial Catalog=dreamcrusherdbtest;User Id=dreamuser;Password=Dre@m*_;Integrated Security=false;";
 
             }
             catch (Exception)
